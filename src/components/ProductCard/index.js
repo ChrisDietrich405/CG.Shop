d7f91e23a0a9423225627125d9a4c14e856cd520
Chris Dietrich <chrisdietrich3666@gmail.com>
2021-04-16 22:44:22 -0400

    added heart functionality
    Co-authored-by:GabrielDVPereira <gdavi.gd@gmail.com>

---
+++ src/components/ProductCard/index.js
@@ -0,0 +1,27 @@
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai"
+
+export default function ProductCard({product, saveFavoriteId, isFavorite, removeFavoriteId}) {
+
+    function formatTitle(text) {
+		if(text.length > 60) {
+			return text.slice(0, 60)+"..."
+		}	
+		return text 
+	}
+
+	function formatPrice(price) {
+		return new Intl.NumberFormat("en-US", {style:"currency", currency:"USD"}).format(price)
+	}    
+
+    return ( 
+    <div className="product-card">
+        <img src={product.url} alt="smartphone" />
+        <p>{formatTitle(product.title)}</p>
+        <div>
+            <span>{formatPrice(product.price)}</span>
+            {isFavorite ? <AiFillHeart size="20px" onClick={() => removeFavoriteId(product.id)} /> : <AiOutlineHeart size="20px" onClick={() => saveFavoriteId(product.id)} />  }
+            
+        </div>
+    </div> 
+    )  
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,31 +2,50 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./styles.css";
 
+import ProductCard from "../../components/ProductCard"
+
 function Home() {
 	const [products, setProducts] = useState([]);
+	const [favoriteProductIds, setFavoriteProductIds] = useState([]); // Array of fav products 
 
-	useEffect(() => {
+	function saveFavoriteId(id) { // add new id to our favoriteProductIds array 
+			setFavoriteProductIds([...favoriteProductIds, id]) // set the new array ids to the state
+	}
+
+	function removeFavoriteId(id) {
+	
+    const newArray = favoriteProductIds.filter((favId) => {
+				return favId !== id
+			});
+
+			setFavoriteProductIds(newArray)
+			
+	}
+
+	useEffect(() => { 
 		axios
 			.get("http://localhost:5000/products")
 			.then((response) => {
-				setProducts(response.data); // response is an object, data is an attribute
+				setProducts(response.data);
 			})
-			.catch((err) => {
+			.catch((err) => { 
 				console.log(err);
-				alert("something is wrong :(");
+				alert("something is wrong :("); //we are going to add toast react later
 			});
-	}, []); //we usually put state variables inside of the brackets,
+	}, []); //we usually put state variables inside of the brackets, it runs once when the pages is loaded
+              //whenever there is something in the brackets, that thing triggers the UseEffect hook
+
 
 	return (
 		<div className="home-container">
 			<h2>Best Smartphones in Town</h2>
 			<div className="products-container">
 				{products.map((product, i) => {
+		
+					const isFavorite = favoriteProductIds.includes(product.id);
+
 					return (
-						<div key={i} className="product-card">
-							<img src={product.url} alt="smartphone" />
-							<p>{product.title}</p>
-						</div>
+						<ProductCard key={i} product={product} isFavorite={isFavorite} removeFavoriteId={removeFavoriteId}saveFavoriteId={saveFavoriteId}/>
 					);
 				})}
 			</div>
