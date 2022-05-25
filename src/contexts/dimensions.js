398e34dfcf0726ab2a94cc9540130545486f094f
Chris <chrisdietrich3666@gmail.com>
2022-05-24 19:47:20 -0500

    improved responsivity for tablet version

--- src/contexts/dimensions.js
+++ src/contexts/dimensions.js
@@ -1,26 +1,34 @@
-import { createContext, useState, useEffect } from "react"  
+import { createContext, useState, useEffect } from "react";
 
-export const DimensionContext = createContext() 
+export const DimensionContext = createContext();
 
-const DimensionProvider = ({children}) => {  
-    const [dimensions, setDimensions] = useState({width: window.innerWidth, height: window.innerHeight})
-     
-    useEffect(() => { 
-        window.addEventListener("resize", () => {   
-            setDimensions({width: window.innerWidth, height: window.innerHeight})
-        })
-        console.log("hello2")
-        return () => {
-            window.removeEventListener("resize") 
-        }
-        
-    },[]) 
-    console.log(dimensions)
-    return (                       
-        <DimensionContext.Provider value={{dimensions, isDeskTop: dimensions.width >= 768, isMobile: dimensions.width < 768}}>
-            {children}
-        </DimensionContext.Provider>
-    )
-}
+const DimensionProvider = ({ children }) => {
+  const [dimensions, setDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-export default DimensionProvider
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    });
+
+    return () => {
+      window.removeEventListener("resize");
+    };
+  }, []);
+  console.log(dimensions);
+  return (
+    <DimensionContext.Provider
+      value={{
+        dimensions,
+        isDeskTop: dimensions.width >= 968,
+        isMobile: dimensions.width < 968,
+      }}
+    >
+      {children}
+    </DimensionContext.Provider>
+  );
+};
+
+export default DimensionProvider;
