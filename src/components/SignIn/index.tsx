696e6f7d99bf738542fd7dd73a257f7c3a15b300
Chris <chrisdietrich3666@gmail.com>
2022-11-03 18:05:23 -0500

    feat added json-server for sign-in

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
 
 import "../../assets/css/form.css";
 
 export default function SignIn() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-  const submit = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const submit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+    try {
+      const response = await api.get(`/users?email=${email}`);
+
+      if (
+        response.data.length > 0 &&
+        response.data[0].password === "password"
+      ) {
+        localStorage.setItem("data", JSON.stringify(response.data));
+      } else {
+        alert("big problems");
+      }
+    } catch (error) {}
   };
+
+  useEffect(() => {
+    const data = localStorage.getItem("data");
+    if (data) {
+     const parsedData = JSON.parse(data);
+     if(parsedData.length > 0) {
+      setIsAuthenticated(true)
+     }
+    }
+
+
+
+  }, []);
 
   return (
     <div className="form-container">
@@ -17,7 +44,12 @@ export default function SignIn() {
         <h3 className="signin">Log In</h3>
         <label htmlFor="email">
           <p className="email">Email or Phone Number</p>
-          <input type="text" id="name" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="text"
+            id="name"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label htmlFor="password">
           <div className="password-container">
