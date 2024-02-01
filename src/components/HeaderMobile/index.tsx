d1a8817ca91d43a6d2e23fed30bebd7ed855575a
Chris <chrisdietrich3666@gmail.com>
2024-02-01 13:30:15 -0500

    removed yarn lock file and updated docker compose file

--- src/components/HeaderMobile/index.tsx
+++ src/components/HeaderMobile/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaShoppingCart, FaHeart } from "react-icons/fa";
 import { CgProfile, CgMenu } from "react-icons/cg";
 import { Link } from "react-router-dom";
